cca1f1759b7c21524fb30b8ca70de2e5a9250e38
nicolasvanheusden <nicolas.vanheusden@resileyes.com>
2022-01-08 16:38:29 +0100

    finish parrot

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,14 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string word;
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> word;
+        cout << "Craow " << word << " !" << endl;
+    }
+    return 0;
+}
